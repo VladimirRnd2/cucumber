e000f3ecdb4c3a7e89529788c52d27401139aff3
vvoronkov <vvoronkov@zuzex.com>
2022-08-02 12:12:41 +0300

    9  Commit

--- src/test/java/com/zuzex/cucumber/SpringIntegrationTest.java
+++ src/test/java/com/zuzex/cucumber/SpringIntegrationTest.java
@@ -2,9 +2,19 @@ package com.zuzex.cucumber;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 import org.junit.runner.RunWith;
+
+import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources")
+@Suite
+@IncludeEngines("cucumber")
+//@SelectClasspathResource("com/planetpope/cucumber")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty,json:target/cucumber.json")
 public class SpringIntegrationTest {
 }
